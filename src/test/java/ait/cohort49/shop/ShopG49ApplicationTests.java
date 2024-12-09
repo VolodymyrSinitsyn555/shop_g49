310a9d1b31b9a41ec7d788be2bb97debb8ee2e43
VolodymyrSinitsyn555 <vladiofun555@gmail.com>
2024-12-09 16:18:44 +0100

    Initial commit

---
+++ src/test/java/ait/cohort49/shop/ShopG49ApplicationTests.java
@@ -0,0 +1,13 @@
+package ait.cohort49.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopG49ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
